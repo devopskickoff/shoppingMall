28823abf44a1a995ef550883bdd7aa5062145299
devopskickoff <devopskick@gmail.com>
2021-12-17 17:38:38 +0900

    commit

--- src/main/java/com/shop/wear/web/HelloController.java
+++ src/main/java/com/shop/wear/web/HelloController.java
@@ -1,13 +1,18 @@
 package com.shop.wear.web;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.wear.constant.ItemSellStatus;
 import com.shop.wear.entity.Item;
+import com.shop.wear.entity.QItem;
 import com.shop.wear.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -15,11 +20,13 @@ public class HelloController {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    EntityManager em;
+
     @GetMapping("/hello")
     public String hello(){
-
-
-        createItemList();
+        //createItemList();
+        queryDsl();
         return "hello";
     }
 
@@ -35,6 +42,22 @@ public class HelloController {
             item.setUpdateTime(LocalDateTime.now());
             Item savedItem = itemRepository.save(item);
             System.out.println(savedItem.toString());
+        }
+    }
+
+    public void queryDsl(){
+        //this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemNm.like("%" + "테스트 상품 상세 설명"+"%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
         }
     }
 }
