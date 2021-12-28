d79a3ab7aa84024313616fcd7955d813bbf19f29
devopskickoff <devopskick@gmail.com>
2021-12-28 17:37:27 +0900

    member form 추가

--- src/main/java/com/shop/wear/controller/MemberController.java
+++ src/main/java/com/shop/wear/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.shop.wear.controller;
 
 import com.shop.wear.dto.MemberFormDto;
+import com.shop.wear.entity.Member;
 import com.shop.wear.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
@@ -14,10 +17,19 @@ import org.springframework.ui.Model;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto",new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String memberForm(MemberFormDto memberFormDto){
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/";
     }
 }
